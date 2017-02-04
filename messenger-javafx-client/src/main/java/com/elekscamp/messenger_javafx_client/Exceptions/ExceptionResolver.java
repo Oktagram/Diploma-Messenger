a74de83549866f3038e70132edc80aa5afcb5daa
Anton_Galko <oktagram98@gmail.com>
2017-02-04 21:35:38 +0200

    Uploading Java client. File extensions limited.

---
+++ messenger-javafx-client/src/main/java/com/elekscamp/messenger_javafx_client/Exceptions/ExceptionResolver.java
@@ -0,0 +1,10 @@
+package com.elekscamp.messenger_javafx_client.Exceptions;
+
+public class ExceptionResolver {
+	public void checkForErrorCodeException(int code, String message) throws HttpErrorCodeException {
+		
+		if (code == 401) throw new HttpErrorCodeException("Only for authorized users.");
+		
+		if (code != -1) throw new HttpErrorCodeException(message);
+	}
+}
