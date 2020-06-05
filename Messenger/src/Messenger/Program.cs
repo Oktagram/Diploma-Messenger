d3d95cfd94a4537f9a47ab45781b00873fedb2ba
Anton Halko <oktagram98@gmail.com>
2020-06-05 15:41:24 +0300

    Set up configs

--- Messenger/src/Messenger/Program.cs
+++ Messenger/src/Messenger/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace Messenger
 {
@@ -8,10 +9,16 @@ namespace Messenger
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("hosting.json", true)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
+                .UseUrls(configuration["urls"])
+                //.UseUrls("http://192.168.98.187:4343")
                 .UseStartup<Startup>()
                 .Build();
 
